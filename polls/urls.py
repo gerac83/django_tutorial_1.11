e65268c42a97bd6c1c092f6731f46ba30e32a3ed
David Manlove <david.manlove@glasgow.ac.uk>
2016-10-28 15:03:28 +0000

    Up to end of tutorial 4

--- polls/urls.py
+++ polls/urls.py
@@ -3,12 +3,8 @@ from polls import views
 
 app_name = 'polls'
 urlpatterns = [
-    # e.g. /polls/
-    url(r'^$', views.index, name='index'),
-    # e.g. /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # e.g. /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # e.g. /polls/5/vote/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
