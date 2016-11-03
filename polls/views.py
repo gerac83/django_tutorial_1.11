0e97740ee0ed25fbdd41ea371f0a98a1838908d3
David Manlove <david.manlove@glasgow.ac.uk>
2016-11-03 15:04:55 +0000

    Up to end of tutorial 5

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 from django.views import generic
 from models import Choice, Question
 
@@ -9,12 +10,16 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last three published questions."""
-        return Question.objects.order_by('-pub_date')[:3]
+        # Return the last three published questions
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:3]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        # Excludes any questions that aren't published yet
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
